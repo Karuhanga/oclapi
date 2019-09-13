44f3410321b99fc672b749fef33d40454823b71b
Lincoln Karuhanga <karuhanga475@gmail.com>
2019-09-13 15:57:05 +0300

    OpenConceptLab/ocl_issues#147: Users can view public collections that are marked private if the user query is specified

--- ocl/collection/filters.py
+++ ocl/collection/filters.py
@@ -1,17 +1,7 @@
 __author__ = 'snyaggarwal'
 
-from oclapi.filters import HaystackSearchFilter
-from django.contrib.auth.models import AnonymousUser
+from oclapi.filters import ConceptContainerPermissionedSearchFilter
 
 
-class CollectionSearchFilter(HaystackSearchFilter):
-    def get_filters(self, request, view):
-        filters = super(CollectionSearchFilter, self).get_filters(request, view)
-        if view.parent_resource:
-            filters.update({'owner': view.parent_resource.mnemonic})
-            filters.update({'ownerType': view.parent_resource.resource_type()})
-
-        if isinstance(request.user, AnonymousUser) or (not request.user.is_staff and view.parent_resource.id not in request.user.get_profile().organizations):
-            filters.update({'public_can_view': True})
-
-        return filters
+class CollectionSearchFilter(ConceptContainerPermissionedSearchFilter):
+    pass
--- ocl/sources/filters.py
+++ ocl/sources/filters.py
@@ -1,19 +1,7 @@
-__author__ = 'misternando'
-
-from oclapi.filters import HaystackSearchFilter
-from django.contrib.auth.models import AnonymousUser
+from oclapi.filters import ConceptContainerPermissionedSearchFilter
 
 __author__ = 'misternando'
 
 
-class SourceSearchFilter(HaystackSearchFilter):
-    def get_filters(self, request, view):
-        filters = super(SourceSearchFilter, self).get_filters(request, view)
-        if view.parent_resource:
-            filters.update({'owner': view.parent_resource.mnemonic})
-            filters.update({'ownerType': view.parent_resource.resource_type()})
-
-        if isinstance(request.user, AnonymousUser) or (not request.user.is_staff and view.parent_resource.id not in request.user.get_profile().organizations):
-            filters.update({'public_can_view': True})
-
-        return filters
+class SourceSearchFilter(ConceptContainerPermissionedSearchFilter):
+    pass
